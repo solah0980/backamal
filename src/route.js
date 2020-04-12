d0603f2eb835f4170898919b0f3e0cf267d1b1eb
unknown <solah0980@gmail.com>
2020-04-12 15:20:20 +0700

    'fix3'

--- src/route.js
+++ src/route.js
@@ -4,11 +4,13 @@ const isAuthen = require('./controller/isAuthen')
 const MyAmal = require('./controller/MyAmal')
 /* let bcrypt = require('bcrypt') */
 module.exports =  (app)=>{
-    app.post('/student/register',Student.Register)
-    app.get('/student/:stdId',Student.Show)
-    app.post('/login',isAuthen.Login)
-    app.post('/myamal',MyAmal.saveAmal)
-    app.post('/showamal',MyAmal.showAmal)
+    app.post('/student/register',(req,res)=>{
+        Student.Register
+    })
+    app.get('/student/:stdId',(req,res)=>Student.Show)
+    app.post('/login',(req,res)=>isAuthen.Login)
+    app.post('/myamal',(req,res)=>MyAmal.saveAmal)
+    app.post('/showamal',(req,res)=>MyAmal.showAmal)
 
     app.get('/test',(req,res)=>{
         res.send("test api")
